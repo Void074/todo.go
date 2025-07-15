615fbabf4a66b95f3b56f26aebf479a9a183ce6a
Gedare Dorke <devpro074@gmail.com>
2025-07-15 17:00:20 +1000

    initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "add a task to the list",
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					fmt.Println("added task >> ", cmd.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:    "complete",
+				Aliases: []string{"c"},
+				Usage:   "complete a task on the list",
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					fmt.Println("completed task", cmd.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
